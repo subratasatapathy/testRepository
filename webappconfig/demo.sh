ee7d8a68f5ec8cadba7d1ae8c184bf1041a91148
root <root@ip-172-42-32-38.ap-south-1.compute.internal>
2019-10-15 06:46:25 +0000

    Test

---
+++ webappconfig/demo.sh
@@ -0,0 +1,9 @@
+
+sudo yum remove docker-ce \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
